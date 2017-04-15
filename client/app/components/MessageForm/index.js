c5379693cde38da290808f74a7a7ac2a257e3bcd
Junichi Ito <jit@sonicgarden.jp>
2017-04-15 14:45:26 +0900

    Enable to add message

--- client/app/components/MessageForm/index.js
+++ client/app/components/MessageForm/index.js
@@ -2,12 +2,17 @@ import React, { Component } from 'react'
 
 // Componentはイベントが使える
 class MessageForm extends Component {
+  onSubmit() {
+    const { actions } = this.props
+    actions.addMessage(this.textMessage.value)
+  }
+
   // メソッド定義
   render() {
     return (
       <div>
-        <textarea rows="4" cols="40" />
-        <input type="button" value="登録" />
+        <textarea ref={(input) => { this.textMessage = input }} rows="4" cols="40" />
+        <input type="button" value="登録" onClick={this.onSubmit.bind(this)} />
       </div>
     )
   }
